0a040abbf61c8e4c5b971542a197879503f8351d
jeffspatafora <jeffspatafora@gmail.com>
2022-04-10 21:58:04 -0700

    adds index action and v-for to display on page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,40 @@
-<script>
+<script>import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      nationalParks: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexNationalParks();
+  },
+  methods: {
+    indexNationalParks: function () {
+      axios.get("/national_parks").then(response => {
+        console.log("indexNationalParks", response.data)
+        this.nationalParks = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div v-for="nationalPark in nationalParks" v-bind:key="nationalPark.id">
+      <h2>{{ nationalPark.name }}</h2>
+      <p>{{ nationalPark.state }}</p>
+      <p>{{ nationalPark.size_square_miles }}</p>
+      <p>{{ nationalPark.date_founded }}</p>
+      <img v-bind:src="nationalPark.map_image" />
+    </div>
   </div>
 </template>
 
 <style>
+img {
+  width: 300px;
+}
 </style>
