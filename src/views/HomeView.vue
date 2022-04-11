51be5546c49727b924a57687af5d469103a9aa09
jeffspatafora <jeffspatafora@gmail.com>
2022-04-10 23:02:09 -0700

    adds create action hardcoded

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,20 @@ export default {
       axios.get("/national_parks").then(response => {
         console.log("indexNationalParks", response.data)
         this.nationalParks = response.data;
-      });
+      })
+    },
+    createNationalPark: function () {
+      console.log("in create");
+      let newNationalParkParams = {
+        name: "Grand Canyon",
+        state: "Arizona",
+        size_square_miles: 1902,
+        date_founded: "02/26/1919",
+        map_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/USA_Arizona_relief_location_map.svg/284px-USA_Arizona_relief_location_map.svg.png"
+      }
+      axios.post("/national_parks", newNationalParkParams).then(response => {
+        console.log(response.date);
+      })
     },
   },
 };
@@ -23,6 +36,18 @@ export default {
 
 <template>
   <div class="home">
+    <h1>New National Parks</h1>
+    <div>
+      <p>Name: <input type="text" /></p>
+      <p>State: <input type="text" /></p>
+      <p>Date Founded: <input type="text" /></p>
+      <p>Size in Square Miles: <input type="text" /></p>
+      <p>Map Image url: <input type="text" /></p>
+      <button v-on:click="createNationalPark()">add a new park</button>
+
+
+    </div>
+    <h1>All National Parks</h1>
     <div v-for="nationalPark in nationalParks" v-bind:key="nationalPark.id">
       <h2>{{ nationalPark.name }}</h2>
       <p>{{ nationalPark.state }}</p>
